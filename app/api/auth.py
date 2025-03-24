86f83080f9c405583dfc0d5081ae343e4fb4d220
eunhoo0903 <eunhoo0903@naver.com>
2025-03-24 20:27:56 +0900

    로그인 라우터 구현

--- app/api/auth.py
+++ app/api/auth.py
@@ -12,11 +12,11 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
 
     existing_username = db.query(User).filter(User.username == user.username).first()
     if existing_username:
-        raise HTTPException(status_code=400, detail="이미 등록된 닉네임입니다.")
+        raise HTTPException(status_code=400, detail="이미 등록된 닉네임 입니다.")
     
     existing_email = db.query(User).filter(User.email == user.email).first()
     if existing_email:
-        raise HTTPException(status_code=400, detail="이미 등록된 이메일입니다.")
+        raise HTTPException(status_code=400, detail="이미 등록된 이메일 입니다.")
     
     hashed_password = get_password_hash(user.password)
     new_user = User(email=user.email, hashed_password=hashed_password, username=user.username)
@@ -25,3 +25,11 @@ def signup(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
+@router.post("/login", response_model=Token, tags=["Auth"])
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.username == user.username).first()
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=400, detail="잘못된 아이디 또는 비밀번호 입니다.")
+    
+    access_token = create_access_token(data={"sub": db_user.email})
+    return {"access_token": access_token, "token_type": "Bearer"}

6ef62ffc5fb7d1d6d2b246818e04ade370c4024c
eunhoo0903 <eunhoo0903@naver.com>
2025-04-17 14:49:18 +0900

    팔로우 언팔로우 기능 추가

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from db.session import Base, engine
-from api import admin, auth, home, emotion, matching
+from api import admin, auth, home, emotion, matching, follow
 
 Base.metadata.create_all(bind=engine)
 
@@ -19,4 +19,5 @@ app.include_router(admin.router)
 app.include_router(auth.router)
 app.include_router(home.router)
 app.include_router(emotion.router)
+app.include_router(follow.router)
 app.include_router(matching.router)

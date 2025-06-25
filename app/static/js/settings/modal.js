593819d916c76d02658e0d3cd3b1c6a04111cb78
eunhoo0903 <eunhoo0903@naver.com>
2025-06-26 00:40:49 +0900

    설정 모달 열기/닫기 기능 구현

---
+++ app/static/js/settings/modal.js
@@ -0,0 +1,8 @@
+document.getElementById("settings-btn").addEventListener("click", () => {
+    document.getElementById("settings-modal").style.display = "block";
+  });
+  
+  document.getElementById("close-settings-btn").addEventListener("click", () => {
+    document.getElementById("settings-modal").style.display = "none";
+  });
+  

b1b1109b1ce0e789b618fdb8c8e1f6da3f9aeec5
eunhoo0903 <eunhoo0903@naver.com>
2025-06-26 12:22:10 +0900

    회원가입/로그인 페이지 내 로그인-회원가입 페이지 전환 버튼 및 로직 구현

--- app/static/js/navigate/navigate.js
+++ app/static/js/navigate/navigate.js
@@ -11,3 +11,17 @@ if (homeBtn) {
     window.location.href = "/home";
   });
 }
+
+const loginBtn = document.getElementById("login-btn");
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    window.location.href = "/login";
+  });
+}
+
+const signupBtn = document.getElementById("signup-btn");
+if (signupBtn) {
+  signupBtn.addEventListener("click", () => {
+    window.location.href = "/signup";
+  });
+}

f9274a907ef3a68916ce628f397c9901d269222a
eunhu0903 <eunhoo0903@naver.com>
2025-07-19 14:49:59 +0900

    이메일 전송용 SMTP 설정 환경변수 추가

--- app/core/config.py
+++ app/core/config.py
@@ -9,5 +9,10 @@ ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 REFRESH_TOKEN_EXPIRE_DAYS = int(os.getenv("REFRESH_TOKEN_EXPIRE_DAYS"))
 
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 if not SECRET_KEY:
     raise ValueError("FastAPI 애플리케이션에 SECRET_KEY가 설정되지 않았습니다.")

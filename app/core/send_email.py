cd57dbacee0eb055c83743acbb4cd1707c77a380
eunhu0903 <eunhoo0903@naver.com>
2025-07-19 14:50:26 +0900

    인증 코드 이메일 전송 유틸 추가 (SMTP 기반)

---
+++ app/core/send_email.py
@@ -0,0 +1,32 @@
+import smtplib
+from email.mime.text import MIMEText
+from core.config import EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+
+def send_verification_email(to_email: str, code: str):
+    subject = "[EmoChat] 이메일 인증 코드"
+    body = f"""
+    안녕하세요!
+
+    EmoChat 이메일 인증 코드는 아래와 같습니다:
+
+    ▶ 인증 코드: {code}
+
+    5분 이내로 입력해주세요.
+
+    감사합니다.
+    """
+
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = EMAIL_HOST_USER
+    msg["TO"] = to_email
+
+    try:
+        server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+        server.starttls()
+        server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+        server.sendmail(EMAIL_HOST_USER, to_email, msg.as_string())
+        server.quit()
+    except Exception as e:
+        raise RecursionError(f"이메일 전송을 실패하였습니다. {e}")
+    

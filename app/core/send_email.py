29d893a4e041d308d87f374321b65adb4159a3f6
eunhu0903 <eunhoo0903@naver.com>
2025-08-02 02:50:45 +0900

    이메일 전송 코드 리팩토링 및 안정성 개선

--- app/core/send_email.py
+++ app/core/send_email.py
@@ -5,28 +5,27 @@ from core.config import EMAIL_HOST, EMAIL_PORT, EMAIL_HOST_USER, EMAIL_HOST_PASS
 def send_verification_email(to_email: str, code: str):
     subject = "[EmoChat] 이메일 인증 코드"
     body = f"""
-    안녕하세요!
+안녕하세요!
 
-    EmoChat 이메일 인증 코드는 아래와 같습니다:
+EmoChat 이메일 인증 코드는 아래와 같습니다:
 
-    ▶ 인증 코드: {code}
+▶ 인증 코드: {code}
 
-    5분 이내로 입력해주세요.
+5분 이내로 입력해주세요.
 
-    감사합니다.
-    """
+감사합니다.
+""".strip()
 
     msg = MIMEText(body)
     msg["Subject"] = subject
     msg["From"] = EMAIL_HOST_USER
-    msg["TO"] = to_email
+    msg["To"] = to_email
 
     try:
-        server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
-        server.starttls()
-        server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
-        server.sendmail(EMAIL_HOST_USER, to_email, msg.as_string())
-        server.quit()
+        with smtplib.SMTP(EMAIL_HOST, EMAIL_PORT) as server:
+            server.starttls()
+            server.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+            server.sendmail(EMAIL_HOST_USER, to_email, msg.as_string())
     except Exception as e:
         raise RecursionError(f"이메일 전송을 실패하였습니다. {e}")
     

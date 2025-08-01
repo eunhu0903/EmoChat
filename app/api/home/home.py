276ee30e0bb73af9b4be13c4b9fdc202cdb67bed
eunhu0903 <eunhoo0903@naver.com>
2025-08-02 03:05:25 +0900

    Home API 리팩토링 및 코드 가독성 개선

--- app/api/home/home.py
+++ app/api/home/home.py
@@ -13,7 +13,7 @@ router = APIRouter()
 def home(authorization: str = Depends(get_token_from_header), db: Session = Depends(get_db)):
     email = verify_token(authorization, db)
 
-    user = db.query(User).filter(User.email == email).first()
+    user = db.query(User).filter(User.email == email).one_or_none()
     if not user:
         raise HTTPException(status_code=401, detail="유저를 찾을 수 없습니다.")
     
@@ -28,10 +28,8 @@ def home(authorization: str = Depends(get_token_from_header), db: Session = Depe
         .all()
     )
 
-    top_emotions = [{"emotion": mood, "count": count} for mood, count in emotion_counts]
-
 
     return {
         "message": f"{user.username}님 환영합니다.",
-        "top_emotions_today": top_emotions
+        "top_emotions_today": [{"emotiom": mood, "count": count} for mood, count in emotion_counts]
     }
